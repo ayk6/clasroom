7f7e1d793a492b05c2638a3693d33caf746dd637
ayKaraca <av.ahmetyasinkaraca@gmail.com>
2022-03-10 00:59:49 +0300

    10.3.22

--- src/day13/Cte_Rte.java
+++ src/day13/Cte_Rte.java
@@ -1,2 +1,22 @@
-package day13;public class Cte_Rte {
+package day13;
+
+public class Cte_Rte {
+    public static void main(String[] args) {
+        //projede hatalı kod varsa projede hiçbir bölüm(class) çalışmaz
+
+        String str = "deneme cümlesi";
+
+        //kodlama hatası varsa uyarı verir ve çalışmaz(syntax[şekilsel] hata)
+        //(compile time error - CTE)
+
+
+        //run time error - RTE
+        //yazımda hata yok ancak çalıştığında hata verebilir (execute)
+
+        System.out.println(str.substring(99));
+        //kodda hata yok ama 99. index olamdığı için çalıştırıldığında hata sonucu verir
+
+
+
+    }
 }
---
+++ src/day14/trim.java
@@ -0,0 +1,16 @@
+package day14;
+
+public class trim {
+    public static void main(String[] args) {
+
+        String str = " Deneme Cümlesi ";
+
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.trim());
+        System.out.println(str.trim().length());
+
+        //String'in başında ve sonundaki boşlukları(space) temizler
+
+    }
+}
