f754a6daae4ae6a31d8ce56d2974b14c909aa1e4
ayKaraca <av.ahmetyasinkaraca@gmail.com>
2022-03-17 23:56:04 +0300

    day21

---
+++ src/day21/arrays2.java
@@ -0,0 +1,32 @@
+package day21;
+
+import java.util.Arrays;
+
+public class arrays2 {
+    public static void main(String[] args) {
+
+        String arr1[]={"asd","qwe","zxcvb"};
+
+        System.out.println(arr1[0]); //ilk veriyi yazdırır "asd"
+
+        arr1[2]="zxc"; //değer değiştirilebilir. 2. index değişti
+        System.out.println(arr1[2]);
+
+        System.out.println(arr1); // [Ljava.lang.String;@5b464ce8 yazdırdı
+        //referans değerleri yazdırır
+
+        //içeriği yazdırmak için
+
+        for (int i=0; i<arr1.length; i++){
+            System.out.print(arr1[i]+" ");  //uzun yöntem
+        }
+        System.out.println("");
+
+        System.out.println(Arrays.toString(arr1)); //köşeli parantez içine yazdırdı
+
+        int arr2[]={1,2,3};
+        arr2[2]=4;
+        System.out.println(Arrays.toString(arr2));
+
+    }
+}
