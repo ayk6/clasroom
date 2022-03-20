52d0637b2a1343ad56c7daae7a9c5b784d2983ac
ayKaraca <av.ahmetyasinkaraca@gmail.com>
2022-03-21 00:24:00 +0300

    day25last

--- src/day25/arrayListPracc3.java
+++ src/day25/arrayListPracc3.java
@@ -18,11 +18,21 @@ public class arrayListPracc3 {
         numbers.add(7.0);
         // ortalamayi bul
 
-        double ortalama=0.0;
-        double toplam=0.0;
+        double average=0.0;
+        double total=0.0;
 
         for (int i=0; i<numbers.size(); i++){
-
+            total += numbers.get(i);
         }
+        average= total /numbers.size();
+        System.out.println(total +"---"+ average); //31.0---3.875
+
+        List<Double> overAverage = new ArrayList<>();
+        for (int i=0; i<numbers.size(); i++){
+            if (numbers.get(i)>average){
+                overAverage.add(numbers.get(i));
+            }
+        }
+        System.out.println(overAverage);
     }
 }
