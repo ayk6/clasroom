cb0d5f1af27e2465ad5b097b62682c656225e65a
ayKaraca <av.ahmetyasinkaraca@gmail.com>
2022-04-15 03:52:04 +0300

    day40reWork

---
+++ src/day40/Exceptions.java
@@ -0,0 +1,25 @@
+package day40;
+
+import java.util.Scanner;
+
+public class Exceptions {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bölmek istediğiniz sayıyı yazın");
+        int num = scan.nextInt();
+
+        System.out.println("Böleni yazın");
+        int num2 = scan.nextInt();
+
+        // System.out.println("Sonuç : "+(num/num2));
+        // 0'a böldüğümüzde Exception verir
+        // Exception devamında hatalı class'ı, methodu ve satırı verir
+        // burada sorunu if ile çözebiliriz
+
+        if (num2 ==0) {
+            System.out.println("tanımsız");
+        }
+        else System.out.println("Sonuç : "+(num/num2));
+    }
+}
