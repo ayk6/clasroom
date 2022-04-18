81afa1677832acac3deb099db5bf300d4f83630e
ayKaraca <av.ahmetyasinkaraca@gmail.com>
2022-04-18 23:37:13 +0300

    day43

---
+++ src/day43/AbstractChild.java
@@ -0,0 +1,16 @@
+package day43;
+
+public class AbstractChild extends AbstractC{
+    // doğrudan extends yapılamaz
+    // override yapmadan cte gösterir
+    // generate override ile düzenlenir
+    // abstract method'lar override olarak eklenir
+    // concrete methodlar isteğe bağlıdır
+    // abstract method child'e eklenince body oluşur
+
+    @Override
+    public void AbstractMethod() {
+
+    }
+    //
+}
