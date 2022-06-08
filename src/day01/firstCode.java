695cf95c7a63e1e9b82747b36d19459b0428d35d
ayKaraca <av.ahmetyasinkaraca@gmail.com>
2022-06-08 15:10:11 +0300

    rew

---
+++ src/day01/firstCode.java
@@ -0,0 +1,17 @@
+package day01;
+
+        public class firstCode {
+             public static void main(String[] args) {
+                 System.out.println("ölüm haritaya giriş yaptı");
+             }
+        }
+
+
+
+
+
+
+
+
+
+
