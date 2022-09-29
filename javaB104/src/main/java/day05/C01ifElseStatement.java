cc7af54dee63a3cec61d3d9d3ffffcf0dfe3d723
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2022-09-29 22:47:48 +0300

    B104day05

--- javaB104/src/main/java/day05/C01ifElseStatement.java
+++ javaB104/src/main/java/day05/C01ifElseStatement.java
@@ -1,8 +1,23 @@
 package day05;
 
+import java.util.Scanner;
+
 public class C01ifElseStatement {
     public static void main(String[] args) {
         // if (boolean) condition
-        // else default condition
+        // else default
+
+        Scanner scan = new Scanner(System.in);
+        int num= scan.nextInt();
+
+        if (num%2==0) System.out.println("even number");
+        else System.out.println("odd number");
+
+        // more than 2 conditions -> else if
+
+        if (num<0) System.out.println("negative");
+        else if (num>0) System.out.println("positive");
+        else System.out.println("neutral");
+
     }
 }
