e1dbe80ab784415e34d749cbdfc07d2a3db8ed29
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2022-10-06 23:45:53 +0300

    pracc day4

---
+++ javaB104/src/main/java/practiceJava/C01variables.java
@@ -0,0 +1,15 @@
+package practiceJava;
+
+public class C01variables {
+    public static void main(String[] args) {
+
+        System.out.println(Integer.MAX_VALUE); //2147483647
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Byte.MIN_VALUE); //-128
+
+        System.out.println(Short.MAX_VALUE); //32767
+
+
+    }
+}
---
+++ javaB104/src/main/java/practiceJava/C02scanner.java
@@ -0,0 +1,24 @@
+package practiceJava;
+
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
+public class C02scanner {
+    public static void main(String[] args) {
+        // \t tab
+        // \n new line
+        // \b backspace
+        System.out.println("i'll \bdelete \bspaces");
+
+        // task - ask user r and calc area and char perimeter of circle
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("r : ");
+        int r = scan.nextInt();
+
+        System.out.println("perimeter : "+(2*3.14*r));
+        System.out.println("area : "+ (3.14*Math.pow(r,2)));
+
+
+    }
+}
