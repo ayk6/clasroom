99b7e00855b62bfa1cb7e634c1cc06ee35e0e1f0
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2022-10-17 23:28:34 +0300

    praccday8

---
+++ javaB104/src/main/java/practiceJava/C07ternary.java
@@ -0,0 +1,28 @@
+package practiceJava;
+
+public class C07ternary {
+    public static void main(String[] args) {
+        int num=555;
+        String digits =
+        (num>99&&num<1000) ? "3": "not 3";
+        System.out.println(digits);
+
+        String day="friday";
+
+        switch (day){
+            case "monday":
+            case "tuesday":
+            case "wednesday":
+            case "thursday":
+            case "friday":
+                System.out.println("mid-week");
+                break;
+            case "saturday":
+            case "sunday":
+                System.out.println("weekend");
+                break;
+            default:
+                System.out.println("invalid");
+        }
+    }
+}
