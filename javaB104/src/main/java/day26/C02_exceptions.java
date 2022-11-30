eb76d98db6bbb81eb897d5ad079957299ee79cda
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2022-11-30 22:34:05 +0300

    b104day26

---
+++ javaB104/src/main/java/day26/C02_exceptions.java
@@ -0,0 +1,56 @@
+package day26;
+
+public class C02_exceptions {
+    public static void main(String[] args) {
+        String str = "125a";
+        System.out.println(method1(str));
+        System.out.println(method2(str));
+        System.out.println(method3(str));
+    }
+
+    private static double method1(String str) {
+        double length= 0;
+        double value = 0;
+        double result = 0;
+        try {
+            length = str.length();
+            value = Integer.parseInt(str);
+            result = (value / length);
+        }catch (NullPointerException e){
+            System.err.println(e.getMessage());
+        }catch (NumberFormatException e){
+            System.err.println(e.getMessage());
+        }catch (ArithmeticException e){
+            System.err.println(e.getMessage());
+        }
+        return result;
+    }
+
+    private static double method2(String str) {
+        double length= 0;
+        double value = 0;
+        double result = 0;
+        try {
+            length = str.length();
+            value = Integer.parseInt(str);
+            result = (value / length);
+        }catch (NullPointerException | NumberFormatException | ArithmeticException e){
+            System.err.println(e.getMessage());
+        }
+        return result;
+    }
+
+    private static double method3(String str) {
+        double length= 0;
+        double value = 0;
+        double result = 0;
+        try {
+            length = str.length();
+            value = Integer.parseInt(str);
+            result = (value / length);
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+        return result;
+    }
+}
