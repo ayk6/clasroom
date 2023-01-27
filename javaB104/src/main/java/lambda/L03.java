f11e755063be9e7aef618028955d97018d0cf9dd
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2023-01-27 18:51:34 +0300

    lambda3last

--- javaB104/src/main/java/lambda/L03.java
+++ javaB104/src/main/java/lambda/L03.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class L03 {
     public static void main(String[] args) {
-        List<String> words = new ArrayList<>(Arrays.asList("firs","sec","3rd","fourth","last","last"));
+        List<String> words = new ArrayList<>(Arrays.asList("first","sec","3rd","fourth","last","last"));
 
         // uppercase, natural sort and print w no repeat (distinct)
         words.stream().map(String::toUpperCase).sorted().distinct().forEach(t-> System.out.print(t+" "));
@@ -21,6 +21,21 @@ public class L03 {
         // check char counts are less than 7
         System.out.println(words.stream().allMatch(t -> t.length() < 7)); // true
         System.out.println(words.stream().allMatch(t -> t.length() < 7)?"true":"false"); // true
+
+        // check all elements don't start w f
+        System.out.println(
+                words.stream().noneMatch(t->t.startsWith("f")) // false
+        );
+
+        // print largest word
+        System.out.println(
+                Arrays.toString(words.stream().sorted(Comparator.comparing(String::length).
+                        reversed()).limit(1).toArray()));
+        // limit returns stream nd we can convert to array, then print
+
+        // sort by last char nd print but skip first word
+        words.stream().sorted(Comparator.comparing(t->t.charAt(t.length()-1)))
+                .skip(1).forEach(L03::printStrWSpace);
 
 
     }
