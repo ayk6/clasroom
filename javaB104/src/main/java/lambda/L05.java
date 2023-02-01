11cdf05d857f8a130d5a6e51ab241bee0d5beaf1
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2023-02-01 23:55:04 +0300

    lambdaEnd

--- javaB104/src/main/java/lambda/L05.java
+++ javaB104/src/main/java/lambda/L05.java
@@ -10,6 +10,14 @@ public class L05 {
 
         sumFirstXEvens(10);
 
+        sumFirstXNums(10);
+
+        printPow2(5);
+
+        printPowGivenNum(3,5);
+
+        factorial(5);
+
     }
 
     public static void sum1toX(int x){
@@ -22,5 +30,23 @@ public class L05 {
 
     private static void sumFirstXEvens(int x) {
         System.out.println(IntStream.iterate(2, t -> t + 2).limit(x).sum());
+    }
+
+    private static void sumFirstXNums(int i) {
+        System.out.println(
+                IntStream.iterate(1,t->t+1).limit(i).sum()
+        );
+    }
+
+    private static void printPow2(int i) {
+        IntStream.iterate(2,t->t*2).limit(i).forEach(L01::printWSpace);
+    }
+
+    private static void printPowGivenNum(int x, int y) {
+        IntStream.iterate(x,t->t*x).limit(y).forEach(L01::printWSpace);
+    }
+
+    private static void factorial(int i) {
+        System.out.println(IntStream.rangeClosed(1, i).reduce(1, (t, u) -> t * u));
     }
 }
