403e8fb07a9419b94c20fdb91f8f97530b3874c1
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2023-05-20 23:56:22 +0300

    ajDay2l2

--- javaB104/src/main/java/advancedJava/generics/Bounding/GenericLowerBound.java
+++ javaB104/src/main/java/advancedJava/generics/Bounding/GenericLowerBound.java
@@ -1,4 +1,29 @@
 package advancedJava.generics.Bounding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericLowerBound {
+    public static void main(String[] args) {
+        List<Object> myObjList = new ArrayList<>();
+        addElements(myObjList);
+        System.out.println(myObjList);
+
+        List<Number> myNumList = new ArrayList<>();
+        addElements(myNumList);
+        System.out.println(myNumList);
+
+        List<Double> myDbList = new ArrayList<>();
+        // addElements(myDbList); RTE : not parent of Int
+    }
+    static void addElements(List<? super Integer> list){
+        // generic but only integer class and parents
+        // ? : wildcard  = unknown type -- useless
+        // super : parents
+
+        for (int i = 1; i<=10; i++){
+            list.add(i);
+        }
+
+    }
 }
---
+++ javaB104/src/main/java/advancedJava/serialization/Serialization_Deserialization.java
@@ -0,0 +1,71 @@
+package advancedJava.serialization;
+
+import java.io.*;
+
+public class Serialization_Deserialization {
+    public static void main(String[] args) {
+
+    }
+
+    private static void writeObj() {
+        System.out.println("object created");
+        User us1 = new User(1001L, "mike", 50);
+        User us2 = new User(1002L, "huel", 40);
+        User us3 = new User(1003L, "lalo", 45);
+
+        try {
+            FileOutputStream fos = new FileOutputStream("user.dat");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(us1);
+            oos.writeObject(us2);
+            oos.writeObject(us3);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class User implements Serializable { // transferable
+    private Long id;
+    private String name;
+    private int age;
+
+    public User(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
