aff83dbfa71b3e365f6e405f6b54e9227bd0de55
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2023-06-04 23:40:29 +0300

    ajDay4v1

---
+++ javaB104/src/main/java/advancedJava/threads/MultiThread02.java
@@ -0,0 +1,32 @@
+package advancedJava.threads;
+
+public class MultiThread02 {
+    public static int counter =0;
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Counter.count();
+                System.out.println("Thread 1 is working");
+            }
+        });
+        thread1.start();
+        thread1.join();
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Counter.count();
+                System.out.println("Thread 2 is working");
+            }
+        });
+        thread2.start();
+    }
+}
+class Counter {
+    public static void count(){
+        for (int i = 0; i<1000;i++){
+            MultiThread02.counter++;
+        }
+        System.out.println(MultiThread02.counter);
+    }
+}
